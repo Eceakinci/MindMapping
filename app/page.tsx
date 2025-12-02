d3c75e01f45797748cd393fa210c6aeb3cef1642
loory <ece12akinci@gmail.com>
2025-12-02 15:38:06 +0100

    added tailwindcss

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
 export default function Page() {
-    return <h1>Hello, Next.js!</h1>
+    return (
+        <>
+            <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
+            <div className='w-24 h-24 bg-rose-800'></div>
+        </>
+
+    )
 }
