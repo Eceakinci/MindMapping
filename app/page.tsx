0151a8588538adf69e43fe429c0f9b7aef5f233b
loory <ece12akinci@gmail.com>
2025-12-03 11:31:45 +0100

    added slider component

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,21 @@
 "use client";
 import data from '../data/data.json'
+import Slider from '../src/components/Slider'
 
 export default function Page() {
     return (
-        <div>
-            {data.data.map(({ article, word, example }) => (
-                <p key={word}>
-                    <strong>{article} {word}</strong> – {example}
-                </p>
-            ))}
+        <div className='flex justify-center m-24'>
+            <Slider
+                width={600}
+                classes={'px-16 pt-6'}
+                items={data.data.map(item => (
+                    <div>
+                        <p className='italic'>{item.type}</p>
+                        <div><strong><span className='text-red-600'>{item.article}</span> {item.word}</strong></div>
+                        <p className='text-green-600'>{item.example}</p>
+                    </div>
+                ))}
+            />
         </div>
     )
 }
---
+++ src/components/Slider.tsx
@@ -0,0 +1,62 @@
+"use client";
+import React, { useState, useRef } from "react";
+
+type SwiperProps = {
+    items: React.ReactNode[];
+    width?: number;
+};
+
+export default function Slider({ items, width = 300, classes }: SwiperProps) {
+    const [index, setIndex] = useState(0);
+    const startX = useRef<number | null>(null);
+
+    const next = () => setIndex((i) => Math.min(i + 1, items.length - 1));
+    const prev = () => setIndex((i) => Math.max(i - 1, 0));
+
+    const handleTouchStart = (e: React.TouchEvent) => {
+        startX.current = e.touches[0].clientX;
+    };
+
+    const handleTouchEnd = (e: React.TouchEvent) => {
+        if (startX.current === null) return;
+
+        const endX = e.changedTouches[0].clientX;
+        const diff = startX.current - endX;
+
+        if (diff > 50) next();
+        else if (diff < -50) prev();
+
+        startX.current = null;
+    };
+    return (
+        <div className='overflow-hidden relative' style={{ width }}>
+            <div className='flex'
+                style={{
+                    transition: "transform 0.3s ease",
+                    transform: `translateX(-${index * width}px)`
+                }}
+                onTouchStart={handleTouchStart}
+                onTouchEnd={handleTouchEnd}>
+                {items.map((item, i) => (
+                    <div key={i} style={{ width }} className={'shrink-0 ' + classes}>
+                        {item}
+                    </div>
+                ))}
+            </div>
+
+            <button
+                onClick={prev}
+                className='absolute top-1/2 left-px'
+                disabled={index === 0}>
+                ◀
+            </button>
+
+            <button
+                onClick={next}
+                className='absolute top-1/2 right-px'
+                disabled={index === items.length - 1}>
+                ▶
+            </button>
+        </div>
+    );
+}
